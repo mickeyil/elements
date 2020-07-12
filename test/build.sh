1bb447dbc9145be7d1aa554cbdb4ed1565891357
Mickey Iluz <mixi.il@gmail.com>
2020-07-12 22:02:28 +0300

    change sim binaries location to bin/

--- test/build.sh
+++ test/build.sh
@@ -7,5 +7,5 @@ SRC_FILES="src/animation_manager.cc src/channel.cc src/requests.cc src/slotsmm.c
 SRC_FILES+="src/op_animation.cc src/sensor.cc src/sensors/distance.cc src/sensor_manager.cc src/op_sensor.cc src/sensors/button.cc src/event_manager.cc src/event.cc "
 SRC_FILES+="src/events/range.cc src/op_event.cc"
 
-g++ -g $CFLAGS -I . -I src/ -o test/sim_anim test/sim_anim.cpp $SRC_FILES -lpaho-mqttpp3 -lpaho-mqtt3as
+g++ -g $CFLAGS -I . -I src/ -o bin/sim_anim test/sim_anim.cpp $SRC_FILES -lpaho-mqttpp3 -lpaho-mqtt3as
 
